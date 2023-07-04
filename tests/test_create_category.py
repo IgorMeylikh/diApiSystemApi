9c2d62df66ca04aa872184963fac0ccae739cbe8
IgorMeylikh <dream_killer2007@mail.ru>
2023-07-04 12:06:43 +0300

    Написал тест проверки доступности эндпоинта.

--- tests/test_create_category.py
+++ tests/test_create_category.py
@@ -1,9 +1,10 @@
 import requests
-from configuration import SERVICE_URL
+from configuration import *
 from src.enums.global_enums import GlobalErrorMessages
-
+from requests.auth import HTTPBasicAuth
 
 def test_getting_posts():
-    response = requests.get(url=SERVICE_URL)
-    assert response.status_code == 404, GlobalErrorMessages.WRONG_STATUS_CODE.value
-    print(response)
+    res = requests.post(url=SERVICE_URL + GETTING_PRODUCTS_PAGE, auth=HTTPBasicAuth('consumer@di-house.ru', 'Password1!'))
+    #response = requests.post(url=SERVICE_URL + GETTING_PRODUCTS_PAGE)
+    assert res.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
+    print(res)
