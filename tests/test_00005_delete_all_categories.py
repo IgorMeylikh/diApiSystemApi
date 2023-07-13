7988eebd1aa0c151b90847faf76850735fa7e99a
IgorMeylikh <dream_killer2007@mail.ru>
2023-07-13 11:17:14 +0300

    Change marker, deleted imports from test del cats

--- tests/test_00005_delete_all_categories.py
+++ tests/test_00005_delete_all_categories.py
@@ -2,9 +2,8 @@ import pytest
 import requests
 from requests.auth import HTTPBasicAuth
 from configuration import SERVICE_URL, CLEAR_CATEGORIES_PAGE, INTERNAL_LOGIN, INTERNAL_PASSWORD, HEADERS
-from src.baseclasses.response import Response
-from src.pydantic_schemas.create_category_pydantic import SuccessResponse, SuccessItem, SuccessStatusCode
 
 #Тест на удаление всех категорий
+@pytest.mark.delete_all_categories
 def test_delete_all_categories(fixture_check_available_endpoint):
     requests.delete(url=SERVICE_URL + CLEAR_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS)
