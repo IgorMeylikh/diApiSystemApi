359a81e85005b8a869fa37d7c58cee065a319524
IgorMeylikh <dream_killer2007@mail.ru>
2023-07-13 12:00:44 +0300

    Создание порядка запуска тестов

--- tests/test_000011_create_category.py
+++ tests/test_000011_create_category.py
@@ -6,35 +6,43 @@ from src.baseclasses.response import Response
 from src.pydantic_schemas.create_category_pydantic import SuccessResponse, SuccessItem, SuccessStatusCode
 
 #Тест на создание категории с передачей 1 валидного элемента
+@pytest.mark.run(order=2)
 def test_create_category(fixture_check_available_endpoint):
-    response = requests.post(url=SERVICE_URL + CREATE_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS, json=ONE_CATEGORY_JSON)
-    test_object = Response(response)
-    test_object.assert_status_code(200)
-    test_object.assert_operation_code('201')
-    test_object.validateTotalSchema(SuccessResponse)
+    if fixture_check_available_endpoint == True: 
+        response = requests.post(url=SERVICE_URL + CREATE_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS, json=ONE_CATEGORY_JSON)
+        test_object = Response(response)
+        # test_object.assert_status_code(200)
+        test_object.assert_operation_code('201')
+        test_object.validateTotalSchema(SuccessResponse)
 
 #Тест на создание категории, которая уже существует системе
-def test_create_repeat_category():
-    response = requests.post(url=SERVICE_URL + CREATE_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS, json=REPEAT_ONE_CATEGORY_JSON)
-    test_object = Response(response)
-    test_object.assert_status_code(200)
-    test_object.assert_operation_code('400')
+@pytest.mark.run(order=2)
+def test_create_repeat_category(fixture_check_available_endpoint):
+    if fixture_check_available_endpoint == True: 
+        response = requests.post(url=SERVICE_URL + CREATE_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS, json=REPEAT_ONE_CATEGORY_JSON)
+        test_object = Response(response)
+        # test_object.assert_status_code(200)
+        test_object.assert_operation_code('400')
 
 #Тест на создание нескольких категорий, одна из этих категорий уже существует (вторая в JSON файле)
-def test_create_validate_category_and_repeat_category():
-    response = requests.post(url=SERVICE_URL + CREATE_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS, json=VALIDATE_CATEGORY_AND_REPEAT_CATEGORY_JSON)
-    test_object = Response(response)
-    test_object.assert_status_code(200)
-    test_object.assert_operation_code('201')
-    test_object.assert_operation_code('400')    
+@pytest.mark.run(order=2)
+def test_create_validate_category_and_repeat_category(fixture_check_available_endpoint):
+    if fixture_check_available_endpoint == True: 
+        response = requests.post(url=SERVICE_URL + CREATE_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS, json=VALIDATE_CATEGORY_AND_REPEAT_CATEGORY_JSON)
+        test_object = Response(response)
+        # test_object.assert_status_code(200)
+        test_object.assert_operation_code('201')
+        test_object.assert_operation_code('400')    
 
-#Тест на отправку создания новой категории и вторым элементом снова создание этой же категории    
-def test_create_validate_category_and_repeat_himself_category():
-    response = requests.post(url=SERVICE_URL + CREATE_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS, json=VALIDATE_CATEGORY_AND_REPEAT_HIMSELF_CATEGORY_JSON)
-    test_object = Response(response)
-    test_object.assert_status_code(200)
-    test_object.assert_operation_code('201')
-    test_object.assert_operation_code('400')   
+#Тест на отправку создания новой категории и вторым элементом снова создание этой же категории  
+@pytest.mark.run(order=2)  
+def test_create_validate_category_and_repeat_himself_category(fixture_check_available_endpoint):
+    if fixture_check_available_endpoint == True: 
+        response = requests.post(url=SERVICE_URL + CREATE_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS, json=VALIDATE_CATEGORY_AND_REPEAT_HIMSELF_CATEGORY_JSON)
+        test_object = Response(response)
+        # test_object.assert_status_code(200)
+        test_object.assert_operation_code('201')
+        test_object.assert_operation_code('400')   
 
 
 
--- tests/test_00005_delete_all_categories.py
+++ tests/test_00005_delete_all_categories.py
@@ -4,6 +4,8 @@ from requests.auth import HTTPBasicAuth
 from configuration import SERVICE_URL, CLEAR_CATEGORIES_PAGE, INTERNAL_LOGIN, INTERNAL_PASSWORD, HEADERS
 
 #Тест на удаление всех категорий
+@pytest.mark.run(order=1)
 @pytest.mark.delete_all_categories
 def test_delete_all_categories(fixture_check_available_endpoint):
-    requests.delete(url=SERVICE_URL + CLEAR_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS)
+    if fixture_check_available_endpoint == True:
+        requests.delete(url=SERVICE_URL + CLEAR_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS)
