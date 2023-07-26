3b8a413c85d51332579baa0aed641279b54671e2
IgorMeylikh <dream_killer2007@mail.ru>
2023-07-26 15:45:43 +0300

    Тест на передачу в качестве значения кода на JS

--- tests/test_product_create.py
+++ tests/test_product_create.py
@@ -1,7 +1,7 @@
 import pytest 
 import requests
 from requests.auth import HTTPBasicAuth
-from configuration import SERVICE_URL, CREATE_PRODUCTS_PAGE, INTERNAL_LOGIN, INTERNAL_PASSWORD, HEADERS, CREATE_ONE_PRODUCT_WITH_OPTIONAL_JSON, CREATE_ONE_PRODUCT_WITHOUT_OPTIONAL_JSON, CREATE_SEVERAL_PRODUCTS_WITH_OPTIONAL_JSON, EMPTY_JSON, CREATE_ONE_PRODUCT_WITHOUT_PRODUCT_SYSTEM_ID_JSON, CREATE_ONE_PRODUCT_WITHOUT_NAME_JSON, CREATE_ONE_PRODUCT_WITHOUT_SKU_JSON, CREATE_ONE_PRODUCT_WITHOUT_TYPE_JSON, CREATE_SEVERAL_PRODUCTS_WITHOUT_OPTIONAL_JSON, CREATE_ONE_SERVICE_WITH_OPTIONAL_JSON, CREATE_ONE_WORK_WITH_OPTIONAL_JSON, CREATE_ONE_PRODUCT_WITH_REPEAT_GUID_JSON, CREATE_ONE_PRODUCT_WITH_REPEAT_SKU_JSON
+from configuration import SERVICE_URL, CREATE_PRODUCTS_PAGE, INTERNAL_LOGIN, INTERNAL_PASSWORD, HEADERS, CREATE_ONE_PRODUCT_WITH_OPTIONAL_JSON, CREATE_ONE_PRODUCT_WITHOUT_OPTIONAL_JSON, CREATE_SEVERAL_PRODUCTS_WITH_OPTIONAL_JSON, EMPTY_JSON, CREATE_ONE_PRODUCT_WITHOUT_PRODUCT_SYSTEM_ID_JSON, CREATE_ONE_PRODUCT_WITHOUT_NAME_JSON, CREATE_ONE_PRODUCT_WITHOUT_SKU_JSON, CREATE_ONE_PRODUCT_WITHOUT_TYPE_JSON, CREATE_SEVERAL_PRODUCTS_WITHOUT_OPTIONAL_JSON, CREATE_ONE_SERVICE_WITH_OPTIONAL_JSON, CREATE_ONE_WORK_WITH_OPTIONAL_JSON, CREATE_ONE_PRODUCT_WITH_REPEAT_GUID_JSON, CREATE_ONE_PRODUCT_WITH_REPEAT_SKU_JSON, CREATE_ONE_SERVICE_WITH_JS_JSON
 from src.baseclasses.response import Response
 from src.pydantic_schemas.create_category_pydantic import CreateCategorySuccessResponse, CreateCategorySuccessItem, CreateCategorySuccessStatusCode
 
@@ -108,6 +108,14 @@ def test_create_work_with_optional_positive():
     test_object = Response(response)
     test_object.assert_status_code(200)
     test_object.assert_operation_code('201')
+
+#Тест с созданием товара когда в качестве одного из значений параметра передаётся JS. На примере картинки для товара
+@pytest.mark.run(order=40)
+def test_create_work_with_js_negative():
+    response = requests.post(url=SERVICE_URL + CREATE_PRODUCTS_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS, json=CREATE_ONE_SERVICE_WITH_JS_JSON)
+    test_object = Response(response)
+    test_object.assert_status_code(200)
+    test_object.assert_operation_code('500')
 
 # Необходимо в тесты добавить сравнение эталонной и возвращаемой схем JSON
 # Для этого для каждого вида запроса, возвращающего свой ответ, написать класс Pydantic, который будет описывать схему ответа JSON
