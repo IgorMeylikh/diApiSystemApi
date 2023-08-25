d523a01197d345b0487317bb23dbad05819d4efd
IgorMeylikh <dream_killer2007@mail.ru>
2023-08-25 15:59:00 +0300

    Обновил имя переменной

--- conftest.py
+++ conftest.py
@@ -1,13 +1,13 @@
 import pytest
 import requests
 from requests.auth import HTTPBasicAuth
-from configuration import SERVICE_URL, CREATE_CATEGORIES_PAGE, INTERNAL_LOGIN, INTERNAL_PASSWORD, HEADERS
+from configuration import SERVICE_URL, CREATE_CATEGORIES_PAGE, INTERNAL_LOGIN, INTERNAL_PASSWORD, INTERNAL_HEADERS
 from src.enums.global_enums import GlobalErrorMessages
 
 #Фикстура проверки доступности сервиса с валидными логином, паролем, заголовками
 @pytest.fixture()
 def fixture_check_available_endpoint():
-    response = requests.post(url=SERVICE_URL + CREATE_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=HEADERS, json={})
+    response = requests.post(url=SERVICE_URL + CREATE_CATEGORIES_PAGE, auth=HTTPBasicAuth(INTERNAL_LOGIN, INTERNAL_PASSWORD), headers=INTERNAL_HEADERS, json={})
     if response.status_code == GlobalErrorMessages.OK_STATUS.value:
         return True
     else:
